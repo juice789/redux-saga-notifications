83d7454bf0dd0cd45206c107f29e6ef2b6ca0971
juice789 <qbyqby@gmail.com>
2023-09-07 13:25:09 +0200

    added option to iconless build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,11 +4,13 @@ import commonjs from '@rollup/plugin-commonjs'
 
 import pkg from './package.json' assert { type: "json" }
 
+const iconless = process.env.iconless
+
 export default {
   input: 'src/index.js',
   output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'esm' }
+    { file: iconless ? pkg.main.replace('index', 'index_iconless') : pkg.main, format: 'cjs' },
+    { file: iconless ? pkg.module.replace('index', 'index_iconless') : pkg.module, format: 'esm' }
   ],
   plugins: [
     babel({
